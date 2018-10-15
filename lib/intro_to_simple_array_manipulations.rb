6a54ecd88cbcf1cccd10de47d3c245a9d45c819e
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-15 18:33:48 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,19 +26,19 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert
-  
+def using_insert(array, element)
+  array.insert[4](element)
 end
 
-def using_uniq
-  
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten
-  
+def using_flatten(array)
+  array.flatten
 end 
 
-def using_delete
-  
+def using_delete(array, string)
+  array.delete(string)
 end
 
